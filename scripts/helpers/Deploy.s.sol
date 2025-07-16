03260161e8b5689aeac31e67993e894f48ac7ed4
Andrey Borzenkov <armoking32@gmail.com>
2025-07-16 18:33:28 +0400

    more scripts

--- scripts/helpers/Deploy.s.sol
+++ scripts/helpers/Deploy.s.sol
@@ -8,20 +8,5 @@ import "../../src/helpers/TargetHelper.sol";
 import "forge-std/Script.sol";
 
 contract Deploy is Script {
-    function run() external {
-        uint256 deployerPk = uint256(bytes32(vm.envBytes("TEST_DEPLOYER")));
-
-        vm.startBroadcast(deployerPk);
-        if (block.chainid == 11155111) {
-            SourceHelper sourceHelper = new SourceHelper();
-            console2.log("SourceHelper deployed to:", address(sourceHelper));
-        } else if (block.chainid == 17000) {
-            TargetHelper targetHelper = new TargetHelper();
-            console2.log("TargetHelper deployed to:", address(targetHelper));
-        } else {
-            revert("Unsupported chain ID");
-        }
-        vm.stopBroadcast();
-        // revert("ok");
-    }
+    function run() external {}
 }
--- scripts/lisk-ethereum/ScriptSource.s.sol
+++ scripts/lisk-ethereum/ScriptSource.s.sol
@@ -5,20 +5,41 @@ pragma solidity 0.8.25;
 import "../Constants.sol";
 import "forge-std/Script.sol";
 
+interface IUrdFactory {
+    function createUrd(
+        address initialOwner,
+        uint256 initialTimelock,
+        bytes32 initialRoot,
+        bytes32 initialIpfsHash,
+        bytes32 salt
+    ) external returns (address urd);
+}
+
 contract Deploy is Script {
-    SourceCore public sourceCore = SourceCore(0x0cFC89E03c52F1F091544De5848EBaFf21148DCc);
+    address public constant URD_FARM_FACTORY = 0xC3da6acf214e322Cc0E07D1582c9a546fB4338da;
+    address public constant RE7_FARM_ADMIN = 0xa62243c7a36e74d8280781242a3B0e019ce74E64;
 
     function run() external {
         uint256 deployerPk = uint256(bytes32(vm.envBytes("TEST_DEPLOYER")));
-        address deployer = vm.addr(deployerPk);
-
+        // address deployer = vm.addr(deployerPk);
         vm.startBroadcast(deployerPk);
-        IERC20 asset = IERC20(sourceCore.asset());
-        Address.sendValue(payable(address(asset)), 0.04 ether);
-        uint256 balance = asset.balanceOf(deployer);
-        // asset.approve(address(sourceCore), type(uint256).max);
-        sourceCore.deposit(balance, deployer);
-        sourceCore.pushToTarget{value: 0.001 ether}();
+        address[3] memory cores = [
+            0x1b10E2270780858923cdBbC9B5423e29fffD1A44,
+            0xa67E8B2E43B70D98E1896D3f9d563f3ABdB8Adcd,
+            0x8cf94b5A37b1835D634b7a3e6b1EE02Ce7F0CD30
+        ];
+        for (uint256 i = 0; i < cores.length; i++) {
+            address core = cores[i];
+            bytes32 salt = keccak256(abi.encode(RE7_FARM_ADMIN, core));
+            address farm = IUrdFactory(URD_FARM_FACTORY).createUrd(RE7_FARM_ADMIN, 0, bytes32(0), bytes32(0), salt);
+            console2.log(
+                "URD farm: SourceCore: %s; URD farm: %s; URD farm admin: %s",
+                SourceCore(core).symbol(),
+                farm,
+                RE7_FARM_ADMIN
+            );
+        }
         vm.stopBroadcast();
+        // revert("ok");
     }
 }
