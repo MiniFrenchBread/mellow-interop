f3e21b07a9b0df414743a2c2dd2a4772060d4def
Anton Iants <yants.anton@gmail.com>
2026-02-04 10:40:19 +0400

    setup 0G

--- scripts/0g-ethereum/Params.sol
+++ scripts/0g-ethereum/Params.sol
@@ -17,14 +17,26 @@ library Params {
     address constant mellowOFTAdapter = address(0);
     address constant mellowOFT = address(0);
 
-    address constant proxyAdmin = 0x3622B8C85C9a4A2ecda005349045FB80912D38f7;
-    address constant vaultAdmin = 0x3622B8C85C9a4A2ecda005349045FB80912D38f7;
-    address constant curator = 0x3622B8C85C9a4A2ecda005349045FB80912D38f7;
-    address constant operator = 0x3622B8C85C9a4A2ecda005349045FB80912D38f7;
-    address constant asset = 0x1Cd0690fF9a693f5EF2dD976660a8dAFc81A109c;
+    address constant helperBot = 0xc0412361882961570d79CE535cB26Fc71bf363A7; // OG helper bot EOA
+    /*
+    Roles on source (0G):
+           vaultAdmin: DEFAULT_ADMIN_ROLE, SET_MAX_AGE_ROLE
+              curator: SET_LIMIT_ROLE
+             operator: PUSH_ROLE
+        oracleUpdater: SET_VALUE_ROLE
 
-    string constant oftName = "OG Vault OFT";
-    string constant oftSymbol = "OG OFT";
+    Roles on target (Ethereum):
+           vaultAdmin: DEFAULT_ADMIN_ROLE, OFT delegate
+             operator: DEPOSIT_ROLE, REDEEM_ROLE, CLAIM_ROLE, PUSH_ROLE
+    */
+    address constant proxyAdmin = 0xEF1c19bDEE7fE61a1B2b98375D2003f57e4B2B8a; // 5/8 Mellow+0g 4+4 Ethereum+0G
+    address constant vaultAdmin = 0xfc7350b0d7a358Db58875148faF3bDEAaFC82911; // 5/8 Mellow+0g 4+4 Ethereum+0G
+    address constant curator = 0xc6eA3826A7a357162d01e22044D845522f62BB4c; // 0G 3/4 
+    address constant operator = helperBot;
+    address constant asset = 0x1Cd0690fF9a693f5EF2dD976660a8dAFc81A109c; // W0G
+
+    string constant oftName = "0G Vault OFT";
+    string constant oftSymbol = "0G OFT";
     string constant vaultName = "Staked 0G";
     string constant vaultSymbol = "st0G";
 
